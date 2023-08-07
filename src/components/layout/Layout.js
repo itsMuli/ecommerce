54b320c3d87daa16e5663087552e40a8f2f713cc
itsMuli <rosemwendwa163@gmail.com>
2023-08-06 22:11:51 -0700

    changes

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from './Header'
-import Footer from '../Footer'
+import Footer from './Footer'
 
 const Layout = ({children}) => {
   return (
     <div>
         <Header/>
-        <main style={{minHeight:'80vh'}}>{children}</main>
+        <main style={{minHeight:'70vh'}}>{children}</main>
         <Footer/>
     </div>
   )
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import Layout from './../components/layout/Layout'
-
+import React from "react";
+import Layout from "../components/layout/Layout";
+import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
 const Contact = () => {
   return (
-    <Layout>
-        <h1>Contact</h1>
+    <Layout title={"Contact us"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
+          <p className="text-justify mt-2">
+            For queries and info about our products feel free to call us anytime we are 24/7
+            avaialible
+          </p>
+          <p className="mt-3">
+            <BiMailSend /> : www.help@nolcstore.com
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : 072-3456789
+          </p>
+          <p className="mt-3">
+            <BiSupport /> : 1800-0000-0000 (toll free)
+          </p>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
