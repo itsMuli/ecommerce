cc4d79806b213fa4b7438a21d57f9491203cd287
itsMuli <rosemwendwa163@gmail.com>
2023-08-22 17:03:10 +0300

    changes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -66,19 +66,45 @@ const Header = () => {
         </li>
           </>) : (
           <>
-            <li className="nav-item">
-          <NavLink onClick={handleLogout} to="/login" className="nav-link" href="#">
-              Logout
-          </NavLink>
-          </li>
+            <li className="nav-item dropdown">
+              <NavLink 
+                className="nav-link dropdown-toggle" 
+                href="#" 
+                role="button" 
+                data-bs-toggle="dropdown" 
+                aria-expanded="false"
+              >
+                {auth?.user?.name}
+              </NavLink >
+              <ul className="dropdown-menu">
+                <li>
+                  <NavLink 
+                    to={`/dashboard/${
+                      auth?.user?.role === 1 ? "admin" : "user"
+                    }`} 
+                    className="dropdown-item"
+                  >
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink 
+                  onClick={handleLogout} 
+                  to="/login" 
+                  className="dropdown-item"
+                >
+                  Logout
+                </NavLink>
+                </li>
+              </ul>
+            </li>
           </>)
         }
-        <li className="nav-item">
-          <NavLink to="/cart" className="nav-link" href="#">
-              Cart(0)
-          </NavLink>
-        </li>
-        
+            <li className="nav-item">
+              <NavLink to="/cart" className="nav-link" href="#">
+                Cart(0)
+              </NavLink>
+            </li>
       </ul>
     </div>
   </div>
