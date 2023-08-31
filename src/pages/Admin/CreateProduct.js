c7eddf02f5de89c4fa08851a79db6c997797f97d
itsMuli <rosemwendwa163@gmail.com>
2023-08-31 15:00:47 +0300

    changes

--- src/pages/Admin/CreateProduct.js
+++ src/pages/Admin/CreateProduct.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import Layout from '../../components/layout/Layout'
-import AdminMenu from '../../components/layout/AdminMenu'
+import React,{useState, useEffect} from 'react';
+import Layout from '../../components/layout/Layout';
+import AdminMenu from '../../components/layout/AdminMenu';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { Select } from 'antd';
+const {Option} = Select
 
 const CreateProduct = () => {
+  const [categories,setCategories] = useState ([]);
+  const [name, setName] = useState ("");
+  const [description, setDescription] = useState ("");
+  const [price, setPrice] = useState ("");
+  const [category, setCategory] = useState ("");
+  const [quantity, setQuantity] = useState ("");
+  const [shipping, setShipping] = useState ("");
+  const [photo, setPhoto] = useState ("");
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting catgeory");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"create product"}>
     <div className='container-fluid m-3 p-3'>
@@ -12,6 +42,56 @@ const CreateProduct = () => {
         </div>
         <div className='col-md-9'>
             <h1>Create Product</h1>
+            <div className='m-1 w-75'>
+              <Select bordered={false}
+              placeholder="Select a category"
+              size='large'
+              showSearch
+              className='form-select mb-3'
+              onChange={(value) => {
+                setCategory(value)
+              }}
+            >
+              {categories?.map((c) => (
+                <Option key={c._id} value={c.name}>
+                  {c.name}
+                </Option>
+              ))}
+              </Select>
+              <div className='mb-3'>
+                <label className='btn btn-outline-secondary col-md-12'>
+                  {photo ? photo.name : "Upload Photo"}
+                <input 
+                  type='file' 
+                  name='photo'
+                  accept='image/*'
+                  onChange={(e) => setPhoto(e.target.files[0])} 
+                  hidden
+                />
+                </label>
+              </div>
+              <div className='mb-3'>
+                {photo && (
+                  <div className='text-center'>
+                    <img 
+                      src={URL.createObjectURL(photo)}
+                      alt='product_photo'
+                      height={"200px"}
+                      className='img img-responsive' 
+                    />
+                  </div>
+                )}
+              </div>
+              <div className='mb-3'>
+                <input
+                  type='text'
+                  value={name}
+                  placeholder='write a name'
+                  className='form-control'
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            </div>
         </div>
     </div>
     </div>
